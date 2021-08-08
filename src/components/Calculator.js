d6b35c035099071dd9e580ae012be33a188b1225
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-08-08 22:11:58 +0200

    added redux for summary component using for of loop in the summaryContainer

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,8 @@
 import { Card } from '@twilio-paste/core';
 import { MenuItemsContainer } from '../containers/MenuItemsContainer';
 import { NewItemFormContainer } from '../containers/NewItemFormContainer';
+import { SummaryContainer } from '../containers/SummaryContainer';
 import { TipSelectContainer } from '../containers/TipSelectContainer';
-import { Summary } from './Summary';
 
 const Calculator = () => {
   return (
@@ -10,7 +10,7 @@ const Calculator = () => {
       <NewItemFormContainer />
       <MenuItemsContainer />
       <TipSelectContainer />
-      <Summary />
+      <SummaryContainer />
     </Card>
   );
 };
