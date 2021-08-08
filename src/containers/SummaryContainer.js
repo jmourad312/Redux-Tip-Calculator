3cbf055e5b4fab3259cef3ea8e606bec29932a91
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-08-08 22:13:59 +0200

    used the reduce function instead of for of loop in summaryContainer

--- src/containers/SummaryContainer.js
+++ src/containers/SummaryContainer.js
@@ -3,11 +3,13 @@ import { Summary } from '../components/Summary'
 
 const mapStateToProps = state => {
     const items = state.items
-    let subtotal = 0;
     
-    for (const item of items) {
-        subtotal += item.quantity * item.price;
-    }
+    // let subtotal = 0;
+    // for (const item of items) {
+    //     subtotal += item.quantity * item.price;
+    // }
+
+    const subtotal = items.reduce((sum,item)=> sum + item.price * item.quantity,0)
 
     const tipAmount = subtotal * (state.tipPercentage/100);
     const total = subtotal + tipAmount;
