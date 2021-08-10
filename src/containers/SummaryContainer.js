6f9e6361ce84ce3cab780faf8e290eb6227a58ca
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-08-10 15:27:00 +0200

    used createSelector from reselect

--- src/containers/SummaryContainer.js
+++ src/containers/SummaryContainer.js
@@ -1,5 +1,6 @@
 import { connect } from "react-redux";
 import { Summary } from '../components/Summary'
+import { selectSubTotal, selectTipAmount, selectTotal } from "../store/items/selectors";
 
 const mapStateToProps = state => {
     const items = state.items
@@ -9,10 +10,13 @@ const mapStateToProps = state => {
     //     subtotal += item.quantity * item.price;
     // }
 
-    const subtotal = items.reduce((sum,item)=> sum + item.price * item.quantity,0)
+    // const subtotal = items.reduce((sum,item)=> sum + item.price * item.quantity,0)
+    // const tipAmount = subtotal * (state.tipPercentage/100);
+    // const total = subtotal + tipAmount;
 
-    const tipAmount = subtotal * (state.tipPercentage/100);
-    const total = subtotal + tipAmount;
+    const subtotal = selectSubTotal(state);
+    const tipAmount = selectTipAmount(state);
+    const total = selectTotal(state);
 
     return {
         subtotal,
