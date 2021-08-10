6a00fb2c79754902af1535487ff6b4a03e147f3c
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-08-10 15:32:12 +0200

    created new selector to get item total

--- src/store/items/selectors.js
+++ src/store/items/selectors.js
@@ -1,15 +1,24 @@
 import { createSelector } from 'reselect';
 
+export const selectItem = (state, props) => {
+  return state.items.find((item) => item.uuid === props.uuid);
+};
+
+export const selectItemTotal = createSelector(
+  [selectItem],
+  (item) => item.price * item.quantity
+);
+
 export const selectItems = (state) => state.items;
 export const selectTipPercentage = (state) => state.tipPercentage;
 
 export const selectSubTotal = createSelector([selectItems], (items) =>
-  items.reduce((sum, item) => sum + (item.price * item.quantity), 0)
+  items.reduce((sum, item) => sum + item.price * item.quantity, 0)
 );
 
 export const selectTipAmount = createSelector(
   [selectSubTotal, selectTipPercentage],
-  (subTotal, tipPercentage) => subTotal * tipPercentage
+  (subTotal, tipPercentage) => subTotal * (tipPercentage / 100)
 );
 
 export const selectTotal = createSelector(
